f659b8defbbe6d0521cfa7f050337a8078b5f225
klintT <kathrasher1@gmail.com>
2016-06-03 17:42:35 -0400

    Initial Commit

---
+++ tinrooftime-menubar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  tinrooftime-menubar
+//
+//  Created by Klint Thrasher on 6/3/16.
+//  Copyright © 2016 Klint Thrasher. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
